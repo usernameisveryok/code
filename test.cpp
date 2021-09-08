775f961d64935bc4f359f52bba5db53ccba7b8c7
lzx <lizhaoxi20020111@163.com>
2021-09-08 23:33:54 +0800

    my first push

---
+++ test.cpp
@@ -0,0 +1,27 @@
+#include <bits\stdc++.h>
+using namespace std;
+void haha()
+{
+    printf("haha");
+}
+
+void print(int a[], int len){
+    for(int i = 0; i < len; i++ )
+        printf("%d",a[i]);
+}
+int main()
+{
+    int a[] = {1,2,3,4,5};
+    //print(a,5);
+    //system("pause");
+    cout << "hello" << endl;
+    vector<int> arr(10,1);
+    for(auto x: arr){
+        cout << x;
+    }
+    vector<vector<int>> darr;
+    auto ddarr = darr;
+    system("pause");
+    
+    return 0;
+}
